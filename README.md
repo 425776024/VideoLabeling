789f60da0080d66955db7ad34974df50385cff85
425776024 <425776024@qq.com>
2022-06-15 11:00:12 +0800

    可以加载导出的标注记录、机器预标注

--- README.md
+++ README.md
@@ -8,15 +8,18 @@ Prepare labels :`./data/label.txt`
 ### step 2
 Copy mp4 files into: `./data/video` dir
 
-### step 3
+### step 3 (Optional, If image annotation is not required)
 Make txt files: python make_input.py (need ffmpeg)
 
 ### step 4
 Chrome open: coin_annotation_tool.html
 
 ### step 5
-Import 3 txt file in webpage:
-`video.txt 、label.txt 、frame.txt`
+Import txt file in webpage:
+- video.txt, Video list
+- label.txt, Label Information
+- frame.txt, Images to be annotated for each video. (Optional, If image annotation is not required)
+- history.txt, Information marked in history. (Exported via `Download` button)
 
 ![](./images/page.png)
 
--- make_input.py
+++ make_input.py
@@ -5,32 +5,38 @@ frame_dir = './data/frame'
 mp4_list = sorted(os.listdir(video_dir))
 mp4_list = [m for m in mp4_list if '.mp4' in m]
 
+# 帧率25
 t_step = 0.04
+#  是否标单图，False只从视频中标注
+labeling_img = False
 
-frame_file = open('./data/frame.txt', 'w')
+if labeling_img:
+    frame_file = open('./data/frame.txt', 'w')
+    frame_file.write(f'URLID,Frame,Time\n')
+
 video_file = open('./data/video.txt', 'w')
-
-frame_file.write(f'URLID,Frame,Time\n')
 video_file.write(f'URLID,URL\n')
 
 for idx, video in enumerate(mp4_list):
-    idx_str = "%.3d" % (idx)
+    # idx_str = "%.5d" % (idx)
+    idx_str = video.replace('.mp4', '')
     mp4_path = os.path.join(video_dir, video)
     video_file.write(f'{idx_str},{mp4_path}\n')
 
-    mp4_name = video.split('.')[0]
-    mp4_frame_dir = os.path.join(frame_dir, mp4_name)
-    if not os.path.exists(mp4_frame_dir):
-        os.mkdir(mp4_frame_dir)
-        os.system(
-            f"ffmpeg -i {mp4_path}  {mp4_frame_dir}/%05d.png"
-        )
+    if labeling_img:
+        mp4_name = video.split('.')[0]
+        mp4_frame_dir = os.path.join(frame_dir, mp4_name)
+        if not os.path.exists(mp4_frame_dir):
+            os.mkdir(mp4_frame_dir)
+            os.system(
+                f"ffmpeg -i {mp4_path}  {mp4_frame_dir}/%05d.png"
+            )
 
-    img_list = sorted(os.listdir(mp4_frame_dir))
-    t = 0
-    for img in img_list:
-        t_str = "%.2f" % (t)
-        img = os.path.join(mp4_frame_dir, img)
-        line = f'{idx_str},{img},{t_str}\n'
-        frame_file.write(line)
-        t += t_step
+        img_list = sorted(os.listdir(mp4_frame_dir))
+        t = 0
+        for img in img_list:
+            t_str = "%.2f" % (t)
+            img = os.path.join(mp4_frame_dir, img)
+            line = f'{idx_str},{img},{t_str}\n'
+            frame_file.write(line)
+            t += t_step
